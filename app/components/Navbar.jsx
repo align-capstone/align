4aef778b3a0c930ba6ee4a134545d32d1ab339a9
Melanie <melaniemohn@users.noreply.github.com>
2017-06-26 17:33:36 -0400

    started incorporating Victory for timeline (#11)
    * experimented with material UI navbar
    
    * started experimenting with Victory line charts
    
    * added a second line to chart
    
    * added scatter plot to Victory chart
    
    * added event handlers to lines and data points
    
    * making the linter happy

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link, browserHistory } from 'react-router'
+
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+// import getMuiTheme from 'material-ui/styles/getMuiTheme'
+// import FlatButton from 'material-ui/FlatButton'
+
+import {AppBar, Tabs, Tab} from 'material-ui'
+
+import WhoAmI from './components/WhoAmI'
+
+import firebase from 'APP/fire'
+
+// Get the auth API from Firebase.
+const auth = firebase.auth()
+
+auth.onAuthStateChanged(user => user || auth.signInAnonymously())
+
+export default function Navbar(props) {
+  return (
+    <AppBar title="align">
+      <WhoAmI auth={auth}/>
+    </AppBar>
+  )
+}
