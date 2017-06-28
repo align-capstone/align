d015da0c9e685a1bef1d43e37bc4c7048681835f
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-28 10:34:55 -0400

    made resources file with notes

---
+++ app/components/Resource.jsx
@@ -0,0 +1,7 @@
+// MPM: I'm thinking this'll be a dumb component that gets contained by / receives props from the goal / milestone / check-in ?
+// OR, should this be a Resources component?  instead of receiving props re: an individual milestone, pass in all of them ??
+
+// either way, the idea is that the user will input a URL, and we'll scrape that URL to render the resource "card"?
+// a resource card should contain: title, thumbnail photo, and a description at the bottom
+  // plus, it (the title and/or photo?) should be a linke:
+  // have a click handler to redirect to that page / open in a new window?
