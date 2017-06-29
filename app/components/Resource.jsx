86e1d012ae7f42f984b7c6db842c142e72f64112
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-28 23:14:59 -0400

    added API key and cleaned up comments, still need to write the actual presentational compoent and move the ajax elsewhere

--- app/components/Resource.jsx
+++ app/components/Resource.jsx
@@ -1,69 +1,48 @@
-// MPM: I'm thinking this'll be a dumb component that gets contained by / receives props from the goal / milestone / check-in ?
-  // so, really just a render function that we'll call for each thing in resources
+// MPM: I'm thinking this'll be a presentational component that we'll call for each thing in resources
 
-// should this be a Resources component?  instead of receiving props re: an individual milestone, pass in all of them ?? [no]
-
+// should this be a Resources component?  instead of receiving props re: an individual milestone, pass in all of them ??
 // either way, the idea is that the user will input a URL, and we'll scrape that URL to render the resource 'card'?
 // a resource card should contain: title, thumbnail photo, and a description at the bottom, and it should link somewhere
+// EITHER WAY, store that information IN THE DATABASE, meaning MOVE the ajax request out of here
 
-// iterate over all the img tags and extract 'href' or whatever
-// ...
+// figured out how to avoid issues with cross-origin requests
+// but, I still think we might want to refactor this as a back-end route
+// especially bc I don't think we want the API key on the front end???
 
-// instead of having back-end API routes, just make ajax request from in here?
-// and then maybe we can refactor to do it with an express route
-// fuck actually we want to do this server-side to avoid cross-origin issues
-// except it's probably the case that that API we want to use will allow CORS
-// scrape each page for URL images, and then in the actual card, make those images link to the resource
-
-// extract the title tag, plus the img tag sources that seem the most appropriate... <img src=....
-// and, don't redo this work every time; store the title and image in the database
+// MOVE THIS AJAX REQUEST
+// don't redo this work every time; store the title and image in the database
+// so, fire this ajax request ONLY when the resource URL gets added or modified (i.e. when we write to db)
+// but, don't have an onChange tied to the resource URL text field... don't want this to fire for incomplete URLS
+// instead, have a plus button by the rest of our resources that opens up a text field
+// onSubmit / onBlur (NOT onChange) sends to db and fires ajax request
 
 import React, {Component} from 'react'
 import $ from 'jquery'
-// import axios from 'axios'
 
 export default class extends Component {
   constructor(props) {
     super()
     this.state = {
       title: ''
+      // thumbnail, description, etc.
     }
   }
 
   componentDidMount() {
-    const resourceUrl = 'http://api.linkpreview.net/?key=123456&q=https://www.google.com/'
-
-    // $(function(resourceUrl) {
-    //   $.ajax({
-    //     type: 'GET',
-    //     url: resourceUrl,
-    //     success: function(preview) {
-    //       const title = preview.title
-    //       console.log(title)
-    //     }
-    //   })
-    // })
-
-    const target = 'https://www.google.com'
+    // eventually pass this resource URL in as props?
+    const target = 'http://vitals.lifehacker.com/get-closer-to-doing-a-handstand-in-12-minutes-1796377028'
 
     $.ajax({
       url: 'http://api.linkpreview.net',
       dataType: 'jsonp',
-      data: {q: target, key: '123456'},
+      data: {q: target, key: '59546c0da716e80a54030151e45fe4e025d32430c753a'},
       success: function(response) {
         console.log(response)
       }
     })
-
-    // MPM ugh ugh axios doesn't support jsonp or whatever for cross-origin requests
-    // axios.get('http://api.linkpreview.net/?key=123456&q=https://www.google.com/')
-    // .then(res => {
-    //   console.log('res from inside Resource component? ', res)
-    //   const title = res.title
-    //   this.setState({ title })
-    // })
   }
 
+  // here, render the actual resource card with title, thumbnail, and description
   render() {
     return (
       <div>
@@ -72,18 +51,3 @@ export default class extends Component {
     )
   }
 }
-
-/*
-let resourceUrl = 'http://api.linkpreview.net/?key=123456&q=https://www.google.com/'
-
-$(function(resourceUrl) {
-  $.ajax({
-    type: 'GET',
-    url: resourceUrl,
-    success: function(preview) {
-      const title = preview.title
-      console.log(title)
-    }
-  })
-})
-*/
