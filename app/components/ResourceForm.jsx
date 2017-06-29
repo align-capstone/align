8baa5845a06e8309aad4b66189f089165104a547
Melanie Mohn <melaniemohn@yahoo.com>
2017-06-29 17:36:44 -0400

    added ajax request tto resource form and got it to write to firebase

--- app/components/ResourceForm.jsx
+++ app/components/ResourceForm.jsx
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
-let resourceRef, titleRef, urlRef, imageRef, descriptionRef
+import firebase from 'APP/fire'
+const db = firebase.database()
+const resourceRef = db.ref('resources')
+// let resourceRef, titleRef, urlRef, imageRef, descriptionRef
 
 import {MuiThemeProvider, getMuiTheme} from 'material-ui/styles'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
@@ -23,48 +26,54 @@ export default class extends Component {
     this.unsubscribe()
   }
 
-  // componentWillReceiveProps(incoming, outgoing) {
-  //   this.listenTo(incoming.fireRef)
+  // move the functions below to our resource component
+  // writeURL = (url) => {
+  //   urlRef.set(url)
   // }
 
-  // instead of (or in addition to?) calling getPageInfo in onBlur function, call it as onSubmit ??
-  getPageInfo = (target) => {
-    // make our ajax request in here
+  // writeTitle = (title) => {
+  //   titleRef.set(title)
+  // }
+
+  // writeImage = (image) => {
+  //   imageRef.set(image)
+  // }
+
+  // writeDescription = (description) => {
+  //   descriptionRef.set(description)
+  // }
+
+  // don't write URL yet... first, make the ajax request
+  // THEN write title, image, and description based on JSON returned by ajax call
+
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      url: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('clicked it!')
+    console.log('state?', this.state.url)
+
+    const target = this.state.url
     $.ajax({
       url: 'http://api.linkpreview.net',
       dataType: 'jsonp',
       data: {q: target, key: '59546c0da716e80a54030151e45fe4e025d32430c753a'},
       success: function(response) {
         console.log(response)
-        // and then write to firebase with the results
-        // writeURL(response.url) etc. etc.
+        let resourceId
+        resourceRef.push(response)
+        // and here we also want to write to the associated milestone etc.
+        // instead of pushing the object, we'll just do .push(), and we'll save it as a variable...
+        // newResourceRef = resourceRef.push()
+        // newResourceID = newResourceRef.id
+        // look at timelines for example
       }
     })
-  }
-
-  writeURL = (url) => {
-    urlRef.set(url)
-  }
-
-  writeTitle = (title) => {
-    titleRef.set(title)
-  }
-
-  writeImage = (image) => {
-    imageRef.set(image)
-  }
-
-  writeDescription = (description) => {
-    descriptionRef.set(description)
-  }
-
-  // don't write URL yet... first, make the ajax request
-  // THEN write title, image, and description based on JSON returned by ajax call
-
-  handleSubmit = (event) => {
-    // event.preventDefault()
-    console.log('clicked it!')
-    console.log(event.target)
   }
 
   render() {
@@ -72,18 +81,16 @@ export default class extends Component {
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <div>
           <h3>Add a resource below</h3>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div>
               <TextField
                 autoFocus
                 hintText='Paste a URL here'
                 floatingLabelText='URL'
-                // value={this.state.url}
-                // onChange={console.log(this.value)} // set state in here??
+                onChange={this.handleChange} // set state in here??
                 // onFocus={console.log('focused lol')}
                 // onBlur={console.log('blurred ', this.value) /* this.getPageInfo(this.value) */ }
                 // onFocusOut={console.log('unfocused ', this.value)}
-                onSubmit={console.log('is this even')}
                 id='url'
               />
             </div>
