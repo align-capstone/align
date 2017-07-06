8116825afce79c9dfe723d6ffad6fcf92feb6f80
Kladky <kladky@users.noreply.github.com>
2017-07-06 13:21:24 -0400

    updated 404 to make it generic

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -6,13 +6,8 @@ const NotFound = props => {
   console.error('NotFound: %s not found (%o)', pathname, props)
   return (
     <div>
-      <h1>Sorry, I couldn't find <pre>{pathname}</pre></h1>
-      <p>The router gave me these props:</p>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre>
+      <h1>Looks like there's no page here</h1>
       <p>Lost? <Link to="/">Here's a way home.</Link></p>
-      <cite>~ xoxo, bones.</cite>
     </div>
   )
 }
